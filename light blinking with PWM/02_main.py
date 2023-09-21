f5103b73bea798f49debfcae6c18b5c0d1be5e63
Prilax <131780053+Prilax@users.noreply.github.com>
2023-09-21 17:46:28 +0530

    pwm folder

---
+++ light blinking with PWM/02_main.py
@@ -0,0 +1,54 @@
+#ESP32 PWM
+#LED fading using PWM
+#PWM can be enam=bled on all output enable pins
+#the base frequency ranges from 1Hz to 40MHz
+
+#but there is a trade off
+#as the base frequency increases, the duty resolution decreases
+
+
+
+from machine import Pin,Timer,PWM
+from time import sleep_ms
+
+# pwm object and its configuration
+#pwm frequency = 1000Hz
+pwm1=PWM(Pin(4),freq=1000)
+pwm2=PWM(Pin(2),freq=1000)
+
+while True:
+    #increase the LED brightness
+    
+    print("Increasing LED brightness...")
+    for i in range (0,1024):
+        pwm1.duty(i)
+        sleep_ms(2)
+        i += 1
+    print(" LED is at its max brightness")
+    
+    # wait at max brightness for 2 sec
+    sleep_ms(2000)
+    # decrese the LED brightness
+    print("Decreasing LED brightness")
+    for i in range(0,1024):
+        pwm1.duty(1023-i)
+        sleep_ms(2)
+        i += 1
+    print("LED is at its min brightness")
+    print("Increasing LED brightness...")
+    for i in range (0,1024):
+        pwm2.duty(i)
+        sleep_ms(2)
+        i += 1
+    print(" LED is at its max brightness")
+    
+    # wait at max brightness for 2 sec
+    sleep_ms(2000)
+    # decrese the LED brightness
+    print("Decreasing LED brightness")
+    for i in range(0,1024):
+        pwm2.duty(1023-i)
+        sleep_ms(2)
+        i += 1
+    print("LED is at its min brightness")
+    #wait at min brightness for 2 sec
